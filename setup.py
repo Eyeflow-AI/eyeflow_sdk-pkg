f6c62e14134366a7537881b23ca5caff3fe8470e
Alex Sobral de Freitas <alexsobral@siliconlife.ai>
2022-03-30 17:28:06 +0000

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="eyeflow_sdk",
-    version="0.9.66",
+    version="0.9.67",
     author="SiliconLife.AI",
     author_email="support@siliconlife.ai",
     description="Functions and classes for development of Eyeflow Applications",
